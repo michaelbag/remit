8b5b89d3cf6c36355d1812d6ac371052ac73dbe6
kma <kma@rhana.com>
2024-09-05 23:01:46 +0300

    Added Account model in ACC application

--- apps/acc/admin.py
+++ apps/acc/admin.py
@@ -1,13 +1,46 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 import apps.acc.models
+
+
+@admin.register(apps.acc.models.Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = [
+        'guid',
+        'name',
+        'employee',
+        'code',
+        'disabled',
+        'is_disabled',
+        'archive',
+        'archived',
+        'in_profiles'
+    ]
+    readonly_fields = ['guid']
+    list_editable = ['archive', 'disabled']
+
+    @staticmethod
+    def in_profiles(obj):
+        #return ', '.join([profile.name for profile in obj.profiles])
+        return ''
+
+    @staticmethod
+    def archived(obj):
+        return 'archived' if obj.archive else '---'
+
+    @staticmethod
+    def is_disabled(obj):
+        return 'disabled' if obj.disabled else '---'
+
+    is_disabled.short_description = _('Disabled')
 
 
 @admin.register(apps.acc.models.AccessProfile)
 class AccessProfileAdmin(admin.ModelAdmin):
     list_display = [
-        'guid',
-        'code',
         'name',
-        'resource'
+        'code',
+        'resource',
+        'guid'
     ]
--- apps/acc/models.py
+++ apps/acc/models.py
@@ -3,11 +3,13 @@ from datetime import date
 from django.db import models
 
 from apps.res.models import Resource
+from apps.org.models import Employee
 from common import models as com_models
 from django.utils.translation import gettext_lazy as _
 
 
 class AccessProfile(com_models.Catalog):
+    name = models.CharField(max_length=150, blank=True)
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE, help_text=_('Resource'), related_name='profiles')
     create_date = models.DateField(default=date.today, null=True, help_text=_('Create date'))
     end_date = models.DateField(null=True, blank=True, help_text=_('Expiry date'))
@@ -17,3 +19,22 @@ class AccessProfile(com_models.Catalog):
 
     class Meta:
         verbose_name = _('Access Profile')
+
+
+class Account(com_models.Catalog):
+    name = models.CharField(max_length=50, blank=True)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, help_text=_('Resource'),
+                                 related_name='accounts', limit_choices_to={'accounts_provider': True})
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, help_text=_('Employee'),
+                                 related_name='accounts')
+    technical = models.BooleanField(default=False, help_text=_('Technical account'))
+    create_date = models.DateField(default=date.today, null=True, help_text=_('Create date'))
+    end_date = models.DateField(null=True, blank=True, help_text=_('Expiry date'))
+    disabled = models.BooleanField(default=False)
+    archive = models.BooleanField(default=False)
+    comment = models.TextField(blank=True)
+    profiles = models.ManyToManyField(AccessProfile, related_name='accounts', blank=True)
+
+    class Meta:
+        verbose_name = _('Account')
+
