e261891279f5cef5d9d7f83fa0cb85b6cac55ef3
mihailkudravcev <mk@remark.pro>
2025-07-02 09:19:32 +0300

    Changes in QRCode view and templates

--- apps/qr/views.py
+++ apps/qr/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from apps.qr.models import QRCode
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 
 def index(request):
     short_code = request.GET.get('c')
-    qr_code = QRCode.objects.get(short_public_code=f"{short_code}")
+    try:
+        qr_code = QRCode.objects.get(short_public_code=f"{short_code}")
+    except ObjectDoesNotExist:
+        return render(request, 'q/not_exist.html')
     return render(request, 'q/index.html', {'short_code': request.GET.get('c'),
                                             'code': qr_code})
