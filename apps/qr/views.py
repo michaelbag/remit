66bca0126c81de8fe7e375d7a33165210291f084
kma <kma@rhana.com>
2025-07-02 22:48:18 +0300

    Trying  to create recursive templates for qr/q. Not success yet.

--- apps/qr/views.py
+++ apps/qr/views.py
@@ -10,6 +10,7 @@ def index(request):
     try:
         qr_code = QRCode.objects.get(short_public_code=f"{short_code}")
     except ObjectDoesNotExist:
-        return render(request, 'q/not_exist.html')
-    return render(request, 'q/index.html', {'short_code': request.GET.get('c'),
-                                            'code': qr_code})
+        return render(request, 'index.html', {'template_name': 'q/not_exist.html'})
+    return render(request, 'index.html', {'short_code': request.GET.get('c'),
+                                            'code': qr_code,
+                                            'template_name': 'q/qr_card.html'})
