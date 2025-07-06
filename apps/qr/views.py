4f138778ecae3c755ef829d1e7a8775acecd6ecf
kma <kma@rhana.com>
2025-07-06 21:38:50 +0300

    started extends admin templates in /q/ view template

--- apps/qr/views.py
+++ apps/qr/views.py
@@ -8,10 +8,6 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 def index(request):
     short_code = request.GET.get('c')
     try:
-        # qr_code = QRCode.objects.get(short_public_code="3EJIte6lTMKNi1OFn6CJFA")[0]
-        # qr_code = QRCode.objects.get(guid_public_code="dc4248b5-eea5-4cc2-8d8b-53859fa08914")
-        # qr_code = QRCode.objects.get(code="TD0000005")
-        # qr_code = QRCode.objects.filter(short_public_code=f"{short_code}")[0]
         qr_code = QRCode.objects.get(short_public_code=f"{short_code}")
     except ObjectDoesNotExist:
         return render(request, 'q/not_exist.html', {'short_code': short_code })
